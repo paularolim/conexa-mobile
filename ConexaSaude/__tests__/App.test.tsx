14344bb37376eb923741e0cae61f0c3d80db8ab3
Paula Rolim <rolim.paula1999@gmail.com>
2023-09-28 18:35:00 -0300

    chore: upload project

---
+++ ConexaSaude/__tests__/App.test.tsx
@@ -0,0 +1,13 @@
+import 'react-native';
+import { it } from '@jest/globals';
+import renderer from 'react-test-renderer';
+
+import { App } from '../src/App';
+
+// Note: import explicitly to use the types shiped with jest.
+
+// Note: test renderer must be required after react-native.
+
+it('renders correctly', () => {
+  renderer.create(<App />);
+});
---
+++ ConexaSaude/src/App.tsx
@@ -0,0 +1,9 @@
+import { Text, View } from 'react-native';
+
+export function App(): JSX.Element {
+  return (
+    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
+      <Text>Hello Wolrd</Text>
+    </View>
+  );
+}
