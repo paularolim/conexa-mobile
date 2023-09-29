199c7cb6ae9d5f1a1374b1d274e6d44541fe219c
Paula Rolim <rolim.paula1999@gmail.com>
2023-09-28 23:19:42 -0300

    style: configure imports

--- ConexaSaude/babel.config.js
+++ ConexaSaude/babel.config.js
@@ -1,3 +1,16 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      'module-resolver',
+      {
+        extensions: ['.ts', '.tsx', '.js', '.jsx', '.json', '.svg', '.png'],
+        alias: {
+          '@assets': './src/assets',
+          '@components': './src/components',
+          '@styles': './src/styles',
+        },
+      },
+    ],
+  ],
 };
