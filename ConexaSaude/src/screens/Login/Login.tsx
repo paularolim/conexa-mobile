e61ddf712cdb6d8ad782ec1b3b9790ddee64a0ea
Paula Rolim <rolim.paula1999@gmail.com>
2023-09-29 22:54:59 -0300

    feat: layout login screen

--- ConexaSaude/src/screens/Login/Login.tsx
+++ ConexaSaude/src/screens/Login/Login.tsx
@@ -1,16 +1,36 @@
-import { View } from 'react-native';
+import { Keyboard, TouchableWithoutFeedback } from 'react-native';
 
-import { Button } from '@components/Button';
-import { Text } from '@components/Text';
+import logo from '@assets/images/logo.png';
+import { Button } from '@components/Button/Button';
+import { Divider } from '@components/Divider/Divider';
+import { Input } from '@components/Input';
 import { useAuth } from '@hooks/useAuth';
+
+import { Container, Logo, Section, Title } from './styles';
 
 export function Login() {
   const { handleLogin } = useAuth();
 
   return (
-    <View style={{ flex: 1, justifyContent: 'center', alignItems: 'center' }}>
-      <Text>Login</Text>
-      <Button onPress={handleLogin}>Entrar</Button>
-    </View>
+    <TouchableWithoutFeedback onPress={() => Keyboard.dismiss()}>
+      <Container>
+        <Section>
+          <Logo source={logo} />
+        </Section>
+
+        <Section space={8}>
+          <Title>Para entrar, utilize as credenciais que você recebeu via e-mail</Title>
+          <Divider />
+        </Section>
+
+        <Section space={16}>
+          <Input label="E-mail" keyboardType="email-address" autoCapitalize="none" />
+          <Input label="Senha" secureTextEntry hasPasswordIcon autoCapitalize="none" />
+
+          <Button onPress={handleLogin}>Entrar</Button>
+          <Button type="ghost">Esqueci a senha</Button>
+        </Section>
+      </Container>
+    </TouchableWithoutFeedback>
   );
 }
