491a907f67f367f8cc1271a5d9ff9aeed7ce4f86
Paula Rolim <rolim.paula1999@gmail.com>
2023-09-29 22:52:58 -0300

    feat: input component

---
+++ ConexaSaude/src/components/Input/Input.tsx
@@ -0,0 +1,37 @@
+import { useState } from 'react';
+import { TouchableOpacity } from 'react-native';
+import { useTheme } from 'styled-components';
+
+import { Icon } from '@components/Icon';
+
+import { Container, Field, FieldContainer, Label } from './styles';
+import { InputProps } from './types';
+
+export function Input({ label, hasPasswordIcon, secureTextEntry, ...rest }: InputProps) {
+  const [hidePassword, setHidePassword] = useState(secureTextEntry);
+
+  const { colors } = useTheme();
+
+  const togglePasswordVisibility = () => {
+    setHidePassword((prev) => !prev);
+  };
+
+  return (
+    <Container>
+      <Label>{label}</Label>
+      <FieldContainer>
+        <Field secureTextEntry={hidePassword} {...rest} />
+        {hasPasswordIcon && (
+          <TouchableOpacity onPress={togglePasswordVisibility}>
+            <Icon
+              name={hidePassword ? 'eye-on' : 'eye-off'}
+              width={25}
+              height={25}
+              fill={colors.gray}
+            />
+          </TouchableOpacity>
+        )}
+      </FieldContainer>
+    </Container>
+  );
+}
---
+++ ConexaSaude/src/components/Input/styles.ts
@@ -0,0 +1,32 @@
+import { TextInput, View } from 'react-native';
+import styled from 'styled-components/native';
+
+import { Text } from '@components/Text';
+
+export const Container = styled(View)`
+  background-color: ${({ theme }) => theme.colors.white};
+  width: 100%;
+  justify-content: center;
+  align-items: flex-start;
+`;
+
+export const Label = styled(Text)`
+  color: ${({ theme }) => theme.colors.gray};
+  font-size: 18px;
+`;
+
+export const FieldContainer = styled(View)`
+  border: 1px solid ${({ theme }) => theme.colors.gray};
+  border-radius: 4px;
+  width: 100%;
+  padding: 8px 16px;
+  flex-direction: row;
+  align-items: center;
+  min-height: 48px;
+`;
+
+export const Field = styled(TextInput)`
+  color: ${({ theme }) => theme.colors.black};
+  flex-grow: 1;
+  padding: 0px;
+`;
