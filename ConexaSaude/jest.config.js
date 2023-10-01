c158d12938a0bbeeff76923bb2dcf9526343ba79
Paula Rolim <rolim.paula1999@gmail.com>
2023-10-01 16:56:38 -0300

    shore: configure jest

--- ConexaSaude/jest.config.js
+++ ConexaSaude/jest.config.js
@@ -1,3 +1,12 @@
 module.exports = {
   preset: 'react-native',
+  collectCoverageFrom: ['src/**/*.tsx', 'src/**/*.ts'],
+  coveragePathIgnorePatterns: ['node_modules', 'interfaces', './src/App.tsx'],
+  testPathIgnorePatterns: ['__tests__/Wrapper.tsx', './__tests__/setupTests.js'],
+  verbose: true,
+  transform: { '^.+\\.svg$': 'jest-transformer-svg' },
+  transformIgnorePatterns: [
+    'node_modules/(?!(jest-)?@?react-native|@react-native-community|@react-navigation)',
+  ],
+  setupFiles: ['./__tests__/setupTests.js'],
 };
