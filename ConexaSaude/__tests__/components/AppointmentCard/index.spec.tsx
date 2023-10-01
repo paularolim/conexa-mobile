254f159a12efa82880e0e48ad60f55a73cdf996c
Paula Rolim <rolim.paula1999@gmail.com>
2023-10-01 19:38:07 -0300

    test: appointment card component

---
+++ ConexaSaude/__tests__/components/AppointmentCard/index.spec.tsx
@@ -0,0 +1,18 @@
+import renderer from 'react-test-renderer';
+
+import { AppointmentCard } from '@components/AppointmentCard';
+
+import { Wrapper } from '../../Wrapper';
+
+describe('Button component', () => {
+  it('should render default component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <AppointmentCard date="2023-10-01 8:30" patient="Mocked Name" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
