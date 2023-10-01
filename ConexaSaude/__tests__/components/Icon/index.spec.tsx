a8e450852cb838ae681ce79f459f0c8049b43adf
Paula Rolim <rolim.paula1999@gmail.com>
2023-10-01 17:01:27 -0300

    test: Icon component

---
+++ ConexaSaude/__tests__/components/Icon/index.spec.tsx
@@ -0,0 +1,73 @@
+import renderer from 'react-test-renderer';
+
+import { Icon } from '@components/Icon';
+
+import { Wrapper } from '../../Wrapper';
+
+describe('Icon component', () => {
+  it('should render Calendar component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="calendar" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render EyeOff component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="eye-off" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render EyeOn component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="eye-on" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render House component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="house" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render Plus component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="plus" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
+  it('should render User component correctly', () => {
+    const tree = renderer
+      .create(
+        <Wrapper>
+          <Icon name="user" />
+        </Wrapper>,
+      )
+      .toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+});
