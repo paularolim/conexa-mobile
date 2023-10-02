701ecd2d3bf6e2c0a820e4a3f2c7dde29e9fac77
Paula Rolim <rolim.paula1999@gmail.com>
2023-10-01 22:15:48 -0300

    feat: add sign out icon

--- ConexaSaude/src/components/Icon/Icon.tsx
+++ ConexaSaude/src/components/Icon/Icon.tsx
@@ -3,6 +3,7 @@ import EyeOff from '@assets/icons/eye-off.svg';
 import EyeOn from '@assets/icons/eye-on.svg';
 import House from '@assets/icons/house.svg';
 import Plus from '@assets/icons/plus.svg';
+import SignOut from '@assets/icons/sign-out.svg';
 import User from '@assets/icons/user.svg';
 
 import { IconProps } from './types';
@@ -21,6 +22,8 @@ export function Icon({ name, width = 40, height = 40, fill = 'none' }: IconProps
       return <EyeOff width={width} height={height} stroke={fill} />;
     case 'eye-on':
       return <EyeOn width={width} height={height} stroke={fill} />;
+    case 'sign-out':
+      return <SignOut width={width} height={height} stroke={fill} />;
     default:
       throw new Error('Icon name is required');
   }
--- ConexaSaude/src/components/Icon/types.ts
+++ ConexaSaude/src/components/Icon/types.ts
@@ -1,4 +1,4 @@
-export type IconName = 'calendar' | 'house' | 'plus' | 'user' | 'eye-on' | 'eye-off';
+export type IconName = 'calendar' | 'house' | 'plus' | 'user' | 'eye-on' | 'eye-off' | 'sign-out';
 
 export interface IconProps {
   name: IconName;
