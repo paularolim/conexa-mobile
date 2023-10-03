b034e10c076d89ded2c566619205a6cee2d15485
Paula Rolim <rolim.paula1999@gmail.com>
2023-10-03 20:43:53 -0300

    feat: configure splash screen and icon on multiplatform

--- ConexaSaude/src/App.tsx
+++ ConexaSaude/src/App.tsx
@@ -1,4 +1,6 @@
+import { useEffect } from 'react';
 import { SafeAreaView } from 'react-native';
+import SplashScreen from 'react-native-splash-screen';
 import { ThemeProvider } from 'styled-components';
 
 import { theme } from '@styles/theme';
@@ -6,6 +8,10 @@ import { theme } from '@styles/theme';
 import { Routes } from './routes';
 
 export function App(): JSX.Element {
+  useEffect(() => {
+    SplashScreen.hide();
+  });
+
   return (
     <ThemeProvider theme={theme}>
       <SafeAreaView style={{ flex: 1 }}>
